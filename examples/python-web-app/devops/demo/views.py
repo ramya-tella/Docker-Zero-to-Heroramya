a9ed3e595af0d236377e0dad5808c3f7a75f174e
ramya-tella <sweetytella14@gmail.com>
2023-04-27 17:19:45 +0530

    changes made

--- examples/python-web-app/devops/demo/views.py
+++ examples/python-web-app/devops/demo/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'demo_site.html')
-    return render(request, 'demo_site.html')
+    return render(request, 'demo1_site.html')
+    return render(request, 'demo1_site.html')
